64137e4e384b465a9c2b8c1e0ba52bccc856e4d6
Bas <bas@bassmit.info>
2021-05-23 16:54:27 +0200

    more examples

--- Assets/Main.cs
+++ Assets/Main.cs
@@ -13,6 +13,10 @@ class Main : MonoBehaviour
     {
         Draw.Sphere(new float3(-2, 0, 0), 1, Color.black, 64);
         Draw.Circle(new float3(3, 0, 0), .7f, _normal, Color.magenta, 64);
+        Draw.Box(1, new float3(-2, 3, 0), quaternion.EulerYXZ(math.radians(new float3(20, 30, 40))), Color.green);
+        Draw.Arrow(new float3(1, 2, 0), new float3(0, 2, 0), Color.white);
+        Draw.Transform(new float3(3, 2.5f, 0), quaternion.RotateY(math.radians(30)));
+
         var a = Speed * Time.deltaTime;
         _normal = math.mul(quaternion.EulerYXZ(a, a, 0), _normal);
     }
@@ -28,7 +32,7 @@ class BurstDrawTestSystem : SystemBase
             .WithBurst()
             .WithCode(() =>
             {
-                Draw.Cone(0, 1, math.radians(20), color);
+                Draw.Cone(new float3(-.2f, -.5f, 0), 1, math.radians(20), color);
             })
             .Schedule();
     }
