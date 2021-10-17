8b9432c8c3c647df00411a787d00d031ac15e26f
Bas <bas@bassmit.info>
2021-10-17 22:25:05 +0200

    minor

--- Assets/GraphTest.cs
+++ Assets/GraphTest.cs
@@ -22,7 +22,6 @@ class GraphTestSystem : SystemBase
         var fix = 1;
         
         Job
-            //.WithBurst()
             .WithCode(() =>
             {
                 var size = fix * new float2(1, 1);
@@ -30,22 +29,23 @@ class GraphTestSystem : SystemBase
                 
                 var pos = new float2(0, 0);
                 var graph = new Draw.Graph(pos, size, -math.PI, math.PI, 1);
-                graph.Plot(new Func0(), 30, Color.red);
-                graph.Plot(new Func2(), 30, Color.green);
+                graph.Plot(new Func0(), 30, Color.red, "SIN");
+                graph.Plot(new Func2(), 30, Color.green, "COS");
                 var a = new NativeArray<float>(2, Allocator.Temp);
                 a[0] = -math.PI / 2;
                 a[1] = math.PI / 2;
-                graph.Plot(new Func1(), 30, Color.blue, a);
+                graph.Plot(new Func1(), 30, Color.blue, "TAN", a);
 
                 pos.x += size.x + border;
                 var s = new GraphSettings(pos, size * 2, -new float2(math.PI, .5f), math.PI, 1f / 3)
                 {
                     MarkingInterval = 2,
-                    HorizontalAxisName = "HORIZONTAL AXIS3!!",
-                    VerticalAxisName = "VERTICALLL"
+                    Title = "THIS IS A TITLE",
+                    HorizontalAxisName = "HORIZONTAL AXIS",
+                    VerticalAxisName = "VERTICAL AXIS"
                 };
                 graph = new Draw.Graph(s);
-                graph.Plot(new Func0(), 30, Color.red);
+                graph.Plot(new Func0(), 30, Color.red, "ALSO SIN");
 
             })
             .Schedule();
